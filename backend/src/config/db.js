d67c284cef2ce210a8aa948320c367f451573341
Vasanth <vasanthda7@gmail.com>
2026-03-19 07:08:44 +0530

    added ssl config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,7 +6,10 @@ const pool = new Pool({
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 pool.connect((err) => {
